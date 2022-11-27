7e12916e8fe628a563aea89e0ff025dd106d34bb
dev-yakuza <dev.yakuza@gmail.com>
2022-11-27 20:11:06 +0900

    Add 0.70.6 version

---
+++ 0.70.6/ch3_FirstApp/babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'babel-plugin-styled-components',
+    [
+      'babel-plugin-root-import',
+      {
+        rootPathPrefix: '~',
+        rootPathSuffix: 'src',
+      },
+    ],
+  ],
+};
